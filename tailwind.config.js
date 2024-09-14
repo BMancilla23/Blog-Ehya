d630db46e76d16afab01442cdbd4034104b2d0bb
Bryan Mancilla <bmancilla119@gmail.com>
2024-09-14 14:53:06 -0500

    chore: configuring tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1565D8",
+        dark: {
+          hard: "#0D2435",
+          soft: "#183956"
+        }
+      },
+      fontFamily: {
+        opensans: ["Open Sans", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
